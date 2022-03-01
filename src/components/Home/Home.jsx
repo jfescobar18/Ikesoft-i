0b1847183695656283fb9bbd693bd0671207382a
Francisco Escobar <jfescobar18@hotmail.com>
2022-03-01 13:35:05 -0600

    ExecutiveContent

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,12 +3,17 @@ import "./styles.scss";
 import Navbar from "../Navbar";
 import Carrousel from "../Carrousel";
 import Spacer from "../Spacer";
+import ExecutiveContent from "../ExecutiveContent";
+import firstBanner from "../../assets/img/ikesoft1.jpg";
 
 const Home = () => {
   return (
     <React.Fragment>
       <Navbar />
       <Carrousel />
+      <Spacer />
+      <ExecutiveContent />
+      
       <Spacer />
     </React.Fragment>
   );

3ff6e9f2fa8a8208cd89a1f41225d4b3e5570a7c
Francisco Escobar <jfescobar18@hotmail.com>
2022-03-01 12:51:57 -0600

    hr added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./styles.scss";
 import Navbar from "../Navbar";
 import Carrousel from "../Carrousel";
+import Spacer from "../Spacer";
 
 const Home = () => {
   return (
     <React.Fragment>
       <Navbar />
       <Carrousel />
+      <Spacer />
     </React.Fragment>
   );
 };
---
+++ src/components/Spacer/Spacer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const Spacer = () => {
+  return (
+    <React.Fragment>
+      <hr className="spacer" />
+    </React.Fragment>
+  );
+};
+
+export default Spacer;

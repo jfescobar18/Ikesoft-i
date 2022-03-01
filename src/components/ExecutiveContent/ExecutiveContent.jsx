014077e3c9b1bfbe10d1aee27d4f4e8f9eed6178
Francisco Escobar <jfescobar18@hotmail.com>
2022-03-01 13:35:10 -0600

    ExecutiveContent

---
+++ src/components/ExecutiveContent/ExecutiveContent.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles.scss";
+
+const ExecutiveContent = () => {
+  return (
+    <React.Fragment>
+      <div className="content-container">
+        <h1>Negocio ejecutivo</h1>
+        <p>
+          Diseño y desarrollo de sistemas de administración y gestión de
+          Negocios/Proyectos.
+        </p>
+        <p>
+          Facturación, compras, ventas, proveedores... Nos adaptamos a tu
+          negocio para digitalizar tus operaciones.
+          <br />
+          <br />
+          Administración y reporteo de métricas del negocio. Sistemas de cobro.
+          E-commerce. Sistemas de envíos.
+        </p>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default ExecutiveContent;
